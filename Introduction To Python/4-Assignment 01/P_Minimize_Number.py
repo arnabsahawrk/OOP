6693795a377ad0b60c9d583168232ed3780e4a71
arnabw <arnabsahawrk@gmail.com>
2025-10-13 09:08:02 +0600

    Assignment 01

---
+++ Introduction To Python/4-Assignment 01/P_Minimize_Number.py
@@ -0,0 +1,21 @@
+# https://codeforces.com/group/MWSDmqGsZm/contest/219774/submission/343295073
+
+N = int(input())
+A = list(map(int, input().split()))
+
+count = 0
+
+while True:
+    f = False
+    for i in range(N):
+        if A[i] & 1:
+            f = True
+            break
+        else:
+            A[i] //= 2
+    if f:
+        break
+    else:
+        count += 1
+
+print(count)
