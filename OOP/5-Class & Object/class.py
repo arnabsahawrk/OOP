7e38a4663878d09883dd0d6d9585fb51fd09ebec
arnabw <arnabsahawrk@gmail.com>
2025-10-13 22:00:47 +0600

    Class & Object

---
+++ OOP/5-Class & Object/class.py
@@ -0,0 +1,14 @@
+class Phone:
+    price = 16000
+    color = "golden"
+    brand = "iPhone"
+    model = 7
+
+
+my_phone = Phone()
+
+print(my_phone)
+print(my_phone.price)
+print(my_phone.color)
+print(my_phone.brand)
+print(my_phone.model)
